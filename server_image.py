1a2b982a9207702c7e030d6e8fd676d5ee3c0a61
kimhyunso <gustn9515@gmail.com>
2023-06-08 21:02:56 +0900

    20230608 21:00

--- server_image.py
+++ server_image.py
@@ -1,79 +1,73 @@
-#!/usr/bin/python
 import socket
 import cv2
 import numpy as np
 from _thread import *
 import base64
 import threading
-import os
-import json
 from datetime import datetime
-import pymysql
 import time
+from util import Util
 
 class Server:
     def __init__(self, HOST, PORT):
         self.HOST = HOST
         self.PORT = PORT
+        self.utility = Util()
         self.count = 0
-        self.lock = threading.Lock()
-        self.path = os.getcwd()
-        self.YEAR = str(datetime.today().year)
-        self.MONTH = str(datetime.today().month)
-        self.DAY = str(datetime.today().day)
-        self.ROOT_PATH = self.path + '\\' + self.YEAR + '\\' + self.MONTH + '\\' + self.DAY
         self.client_sockets = []
         self.server_open()
 
-    def server_open(self):
+
+    def server_open(self) -> None:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 10)
         self.server_socket.bind((self.HOST, self.PORT))
         self.server_socket.listen()
         print('>> Server start')
         self.server_accept()
 
-    def server_accept(self):
+    def server_accept(self) -> None:
         while True:
             try:
                 print('>> Server Wait!!')
-                self.lock.acquire()
                 self.client_socket, self.addr = self.server_socket.accept()
+                self.utility.create_folder(self.addr[0])
                 self.client_socket.settimeout(100)
                 thread = threading.Thread(target=self.receiveTarget)
                 thread.start()
-                self.lock.release()
-                self.createFolder()
             except Exception as e:
                 print(e)
                 self.client_socket.close()
 
     def receiveTarget(self):
         print('>> Connected by :', self.addr[0], ':', self.addr[1])
-        image_count = 0
+        img_count = 0
         while True:
             try:
-                length = self.recvall(self.client_socket ,64)
+                # length = self.recvall(self.client_socket, 64).decode('utf-8')
+                screen_shot = self.recvall_Test()
 
-                if length != None:
-                    decode_length = length.decode('utf-8')
-                    stringData = self.recvall(self.client_socket, int(decode_length))
-                    stime = self.recvall(self.client_socket, 64).decode('utf-8')
-                    print(stime)
+                # length = self.recvall(self.client_socket ,64)
+                # cam_img = self.recvall_Test()
+ 
+                # nowtime = self.recvall_Test()
+                # print(nowtime)
+                # info_length = self.recvall(self.client_socket, 64)
 
-                    info_length = self.recvall(self.client_socket, 64)
+                # info_decode_length = info_length.decode('utf-8')
+                # infomation = self.recvall_Test()
 
-                    if info_length != None:
-                        info_decode_length = info_length.decode('utf-8')
-                        infomation = self.recvall(self.client_socket, int(info_decode_length))
-                        print(json.loads(infomation))
+                screen_shot_data = np.frombuffer(base64.b64decode(screen_shot), dtype='uint8')
+                # cam_img_data = np.frombuffer(base64.b64decode(cam_img), dtype='uint8')
+                
+                decimg = cv2.imdecode(screen_shot_data, 1)
+                cv2.imwrite(self.utility.get_path(self.addr[0]) + '\\' + f'screen_shot_{img_count}.png', decimg)
+                # decimg = cv2.imdecode(cam_img_data, cv2.IMREAD_UNCHANGED)
 
-                        data = np.frombuffer(base64.b64decode(stringData), dtype='uint8')
-                        decimg = cv2.imdecode(data, 1)
-                        cv2.imwrite(self.ROOT_PATH + '\\' + str(self.addr[0]) + '\\' + f'{self.addr[0]}_{image_count}.png', decimg)
-                        print(self.addr[0])
-                        image_count += 1
-                time.sleep(1)
+                # cv2.imwrite(self.ROOT_path + '\\' + str(self.addr[0]) + '\\' + f'{self.addr[0]}_{img_count}.png', decimg)
+
+                img_count += 1
+                time.sleep(0.95)
 
             except Exception as e:
                 print(e)
@@ -81,7 +75,7 @@ class Server:
                 self.client_socket.close()
                 break
 
-    def recvall(self, sock, count):
+    def recvall(self, sock, count) -> bytes:
         buf = b''
         while count:
             newbuf = sock.recv(count)
@@ -90,13 +84,17 @@ class Server:
             count -= len(newbuf)
         return buf
     
-    def createFolder(self):
-        try:
-            path = os.getcwd()
-            if not os.path.exists(self.ROOT_PATH + '\\' + str(self.addr[0])):
-                os.makedirs(self.ROOT_PATH + '\\' + str(self.addr[0]))
-        except OSError:
-            print('Error : Creating directory')
+
+    def recvall_Test(self):
+        data = b""
+        while True:
+            packet = self.client_socket.recv(4096)
+            if not packet:
+                break
+            data += packet
+        return data
+
+    
 
 host_name = socket.gethostname()
 HOST = socket.gethostbyname(host_name)
---
+++ util.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime
+import json
+
+class Util:
+    def __init__(self):
+        self.__path = os.getcwd()
+        self.YEAR = datetime.now().year
+        self.MONTH = '%02d' % datetime.now().month
+        self.DAY = '%02d' % datetime.now().day
+        self.YEAR, self.MONTH, self.DAY = list(map(str, [self.YEAR, self.MONTH, self.DAY]))
+
+    def create_folder(self, ip) -> None:
+        try:
+            if not os.path.exists(self.get_path()):
+                os.makedirs(self.get_path())
+        except OSError:
+            print('Error : Creating directory')
+
+    def get_path(self, ip):
+        return self.get_pwd_path() + '\\' + self.YEAR + '\\' + self.MONTH + '\\' + self.DAY + '\\' + ip
+    
+    def get_path(self):
+        return self.get_pwd_path() + '\\' + self.YEAR + '\\' + self.MONTH + '\\' + self.DAY
+    
+    def get_pwd_path(self):
+        return self.__path
+    
+    
